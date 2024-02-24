560f0dab6d3aadcb3c44448559b7f1c2dc7cd5ca
subham007-coder <dsubham109@gmail.com>
2024-02-24 21:09:39 +0530

    add

---
+++ calculator.js
@@ -0,0 +1,22 @@
+let string = "";
+let buttons = document.querySelectorAll('.btn');
+
+Array.from(buttons).forEach((btn) => {
+    btn.addEventListener('click', (event)=>{
+        if (event.target.innerHTML == '='){
+            string = eval(string);
+            document.querySelector('input').value = string;
+        }
+
+        else  if (event.target.innerHTML == 'AC'){
+            string = "";
+            document.querySelector('input').value = string;
+        }
+
+        else{
+            console.log(event.target);
+            string= string + event.target.innerHTML;
+            document.querySelector('input').value = string;
+        }
+    })
+})
