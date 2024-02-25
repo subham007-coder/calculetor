53629924e9692e28662c6c95e6da51918c92108d
subham007-coder <dsubham109@gmail.com>
2024-02-25 11:41:04 +0530

    add some style

---
+++ script.js
@@ -0,0 +1,24 @@
+// let name = prompt("what is your name?");
+// let nameShow = document.querySelector(".text-center");
+
+// nameShow.innerHTML = `Calculate Me ${name}!`;
+
+let string = "";
+let buttons = document.querySelectorAll('.button');
+Array.from(buttons).forEach((button)=>{
+  button.addEventListener('click', (e)=>{
+    if(e.target.innerHTML == '='){
+      string = eval(string);
+      document.querySelector('input').value = string;
+    }
+    else if(e.target.innerHTML == 'C'){
+      string = ""
+      document.querySelector('input').value = string;
+    }
+    else{ 
+    console.log(e.target)
+    string = string + e.target.innerHTML;
+    document.querySelector('input').value = string;
+      }
+  })
+})
